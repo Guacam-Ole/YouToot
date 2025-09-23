e6b215219e970834a2d72dd340ab905f06ed4861
Ole Albers <ole@nocarrier.de>
2025-09-23 22:27:02 +0200

    serilog+grafana

--- YouToot/Program.cs
+++ YouToot/Program.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NReco.Logging.File;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Grafana.Loki;
 
 namespace YouToot;
 
@@ -31,18 +35,33 @@ public class Program
     {
         var services = new ServiceCollection();
 
-        services.AddLogging(logging =>
+      
+        services.AddLogging(cfg => cfg.SetMinimumLevel(LogLevel.Debug));
+        services.AddSerilog(cfg =>
         {
-            logging.ClearProviders();
-            logging.AddConsole();
-            logging.SetMinimumLevel(LogLevel.Debug);
-            var logFile = "youtoot.log";
-            logging.AddFile(logFile, conf =>
+            cfg.MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("job", Assembly.GetEntryAssembly()?.GetName().Name)
+                .Enrich.WithProperty("service", Assembly.GetEntryAssembly()?.GetName().Name)
+                .Enrich.WithProperty("desktop", Environment.GetEnvironmentVariable("DESKTOP_SESSION"))
+                .Enrich.WithProperty("language", Environment.GetEnvironmentVariable("LANGUAGE"))
+                .Enrich.WithProperty("lc", Environment.GetEnvironmentVariable("LC_NAME"))
+                .Enrich.WithProperty("timezone", Environment.GetEnvironmentVariable("TZ"))
+                .Enrich.WithProperty("dotnetVersion", Environment.GetEnvironmentVariable("DOTNET_VERSION"))
+                .Enrich.WithProperty("inContainer",
+                    Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"))
+                .WriteTo.GrafanaLoki(Environment.GetEnvironmentVariable("LOKIURL") ?? "http://thebeast:3100",
+                    propertiesAsLabels: ["job"]);
+            if (Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration ==
+                "Debug")
             {
-                conf.Append = true;
-                conf.MaxRollingFiles = 1;
-                conf.FileSizeLimitBytes = 100000;
-            });
+                cfg.WriteTo.Console(new RenderedCompactJsonFormatter());
+            }
+            else
+            {
+                cfg.WriteTo.Console();
+            }
         });
         services.AddSingleton<Database>();
         services.AddScoped<Tube>();
